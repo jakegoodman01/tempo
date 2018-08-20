05f5ee8268bccc2ca09694535ca2902d87d0c875
Jake Goodman <jakewgoodman@gmail.com>
2018-08-19 20:31:41 -0400

    Wrote method get_square_from_notation

--- tempo/board.py
+++ tempo/board.py
@@ -6,3 +6,9 @@ class Board:
         # Rows are called 'ranks' and columns are called 'files'
         # Rows are numbered 1 to 8 and columns are lettered a to h
         self._squares: List[List[str]] = [[f'{file}{i}' for file in 'abcdefgh'] for i in range(1, 9)]
+
+    def get_square_from_notation(self, square: str):
+        file = 'abcdefgh'.index(square[0])
+        rank = int(square[1]) - 1
+        return self._squares[rank][file]
+
