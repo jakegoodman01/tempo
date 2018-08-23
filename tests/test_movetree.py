af1fd9ffd4051d135f22dac29fd0e2c70ef9d7f6
Jake Goodman <jakewgoodman@gmail.com>
2018-08-22 20:59:18 -0400

    Wrote tests for MoveTree

--- tests/test_movetree.py
+++ tests/test_movetree.py
@@ -18,14 +18,37 @@ class MoveTreeNodeTests(TestCase):
     def test_next_setter_and_getter(self):
         n = movetree.MoveTreeNode('b8')
         n.next = 'a7'
-        self.assertEqual(n.next, 'a7')
+        self.assertEqual(n.next, movetree.MoveTreeNode('a7'))
 
 
 class MoveTreeHeadTests(TestCase):
     def test_next_getter(self):
         h = movetree.MoveTreeHead('c4')
-        self.assertEqual(h.next, set())
+        self.assertEqual(h.next, list())
 
         h.add_child('b3')
         h.add_child('d3')
-        self.assertEqual(h.next, {'b3', 'd3'})
+        self.assertEqual(h.next, ['b3', 'd3'])
+
+
+class MoveTreeTests(TestCase):
+    mt = movetree.MoveTree('d4')
+    mt.head.add_child('c3')
+    mt.head.add_child('c5')
+    mt.head.add_child('e3')
+    mt.head.add_child('e5')
+
+    def test_next_square_from_head(self):
+        MoveTreeTests.mt.next_square_from_head(2)
+        self.assertEqual(MoveTreeTests.mt.token_node, movetree.MoveTreeHead('e3'))
+
+    def test_back_to_head(self):
+        MoveTreeTests.mt.back_to_head()
+        self.assertEqual(MoveTreeTests.mt.token_node, MoveTreeTests.mt.head)
+
+    def test_next_square(self):
+        MoveTreeTests.mt.back_to_head()
+        MoveTreeTests.mt.next_square_from_head(1)
+        MoveTreeTests.mt.token_node.next = 'b6'
+        MoveTreeTests.mt.next_square()
+        self.assertEqual(MoveTreeTests.mt.token_node, movetree.MoveTreeNode('b6'))
